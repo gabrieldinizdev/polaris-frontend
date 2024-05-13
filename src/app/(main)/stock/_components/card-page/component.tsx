b5a50ea6f65988fe1eec70e6306d98b8f32b8e5c
Dhyon Keyllo <dhyon.kpm@hotmail.com>
2024-05-13 20:48:05 -0300

    refact(app): remove unnused input search component

--- src/app/(main)/stock/_components/card-page/component.tsx
+++ src/app/(main)/stock/_components/card-page/component.tsx
@@ -2,7 +2,6 @@
 
 import { Stack } from "@mui/material";
 
-import { InputSearch } from "@/components/search";
 import type { ComponentCommonProps } from "@/types/common/component";
 
 import { StyledPaper } from "./styled";
@@ -12,7 +11,6 @@ type CardPageProps = {} & ComponentCommonProps;
 export const CardPage = ({ children }: CardPageProps) => {
   return (
     <Stack flexGrow={1}>
-      <InputSearch label="Procurar produto" />
       <StyledPaper>{children}</StyledPaper>
     </Stack>
   );
